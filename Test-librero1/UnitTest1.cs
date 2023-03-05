2bf9f9131e35ff355177125de90c8283923e1931
tamagoru <marushannnn@outlook.com>
2023-03-05 19:51:35 -0300

    Add project files.

---
+++ Test-librero1/UnitTest1.cs
@@ -0,0 +1,144 @@
+using librero1;
+
+namespace Test_librero1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SiLaCantidadDeBasesEsDiferenteALaDeLibrosTiraUnError()
+        {
+            var bases = new List<int>() { 1 };
+            var libros = new List<int>() { 1, 2 };
+            Assert.ThrowsException<Exception>(() =>
+            {
+                Main.Librero(bases, libros);
+            });
+        }
+
+        [TestMethod]
+        public void Caso1()
+        {
+            var bases = new List<int>() { 1, 2 };
+            var libros = new List<int>() { 1, 2 };
+            var resultado = Main.Librero(bases, libros);
+            Assert.AreEqual(3, resultado);
+        }
+
+        [TestMethod]
+        public void Caso2()
+        {
+            var bases = new List<int>() { 1, 4 };
+            var libros = new List<int>() { 1, 4 };
+            var resultado = Main.Librero(bases, libros);
+            Assert.AreEqual(5, resultado);
+        }
+
+        [TestMethod]
+        public void Caso3()
+        {
+            var bases = new List<int>() { 1, 4 };
+            var libros = new List<int>() { 4, 1 };
+            var resultado = Main.Librero(bases, libros);
+            Assert.AreEqual(5, resultado);
+        }
+
+        [TestMethod]
+        public void Caso4()
+        {
+            var bases = new List<int>() { 1, 4, 5 };
+            var libros = new List<int>() { 9, 6, 5 };
+            var resultado = Main.Librero(bases, libros);
+            Assert.AreEqual(10, resultado);
+        }
+
+        [TestMethod]
+        public void Caso5()
+        {
+            var bases = new List<int>() { 1, 2 };
+            var libros = new List<int>() { 4, 1 };
+            var resultado = Main.Librero(bases, libros);
+            Assert.AreEqual(-1, resultado);
+        }
+
+        [TestMethod]
+        public void Ejemplo1()
+        {
+            var bases = new List<int>() { 3, 2, 6, 2, 4, 2, 1, 6 };
+            var libros = new List<int>() { 6, 8, 4, 8, 7, 9, 8, 4 };
+            var resultado = Main.Librero(bases, libros);
+            Assert.AreEqual(10, resultado);
+        }
+
+        [TestMethod]
+        public void Ejemplo2()
+        {
+            var bases = new List<int>() { 1, 1, 1, 21 };
+            var libros = new List<int>() { 4, 4, 4, 4 };
+            var resultado = Main.Librero(bases, libros);
+            Assert.AreEqual(-1, resultado);
+        }
+
+        [TestMethod]
+        public void Casoprueba()
+        {
+            var bases = new List<int>() { 2, 2, 2 };
+            var libros = new List<int>() { 8, 9, 7 };
+            var resultado = Main.Librero(bases, libros);
+            Assert.AreEqual(-1, resultado);
+        }
+
+        [TestMethod]
+        public void Test1PequenioQueDaOk()
+        {
+            var bases = new List<int>();
+            var libros = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                bases.Add(4);
+                libros.Add(6);
+            }
+            var resultado = Main.Librero(bases, libros);
+            Assert.AreEqual(10, resultado);
+        }
+        [TestMethod]
+        public void Test2MedianoQueDaOk()
+        {
+            var bases = new List<int>();
+            var libros = new List<int>();
+            for (int i = 0; i < 1000; i++)
+            {
+                bases.Add(4);
+                libros.Add(6);
+            }
+            var resultado = Main.Librero(bases, libros);
+            Assert.AreEqual(10, resultado);
+        }
+        [TestMethod]
+        public void Test3GrandeQueDaOk()
+        {
+            var bases = new List<int>();
+            var libros = new List<int>();
+            for (int i = 0; i < 10000; i++)
+            {
+                bases.Add(4);
+                libros.Add(6);
+            }
+            var resultado = Main.Librero(bases, libros);
+            Assert.AreEqual(10, resultado);
+        }
+        [TestMethod]
+        public void Test4MuyGrandeQueDaOk()
+        {
+            var bases = new List<int>();
+            var libros = new List<int>();
+            for (int i = 0; i < 100000; i++)
+            {
+                bases.Add(4);
+                libros.Add(6);
+            }
+            var resultado = Main.Librero(bases, libros);
+            Assert.AreEqual(10, resultado);
+        }
+    }
+}
---
+++ librero1/Main.cs
@@ -0,0 +1,34 @@
+﻿namespace librero1
+{
+    public class Main
+    {
+        public static int Librero(List<int> bases, List<int> libros)
+        {
+            if (bases.Count != libros.Count)
+                throw new Exception("cantidad de bases diferente a la cantidad de libros");
+
+            int expected = (bases.Sum() + libros.Sum()) / bases.Count;
+
+            for(int i = 0; i < bases.Count; i++)
+            {
+                bool found = false;
+
+                for (int j = 0; j < libros.Count; j++)
+                {
+                    int x = bases[i] + libros[j];
+
+                    if (x == expected)
+                    {
+                        libros[j] = -1;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) { return -1; }
+            } 
+
+            return expected;
+        }
+    }
+}
